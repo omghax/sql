3bdc0a594f1186192212561f75668be8525386c9
Dray Lacy <dray@izea.com>
2008-07-17 23:07:18 -0400

    DRYed up the statement tests a bit by extracting node creation into helper methods.

--- test/test_statement.rb
+++ test/test_statement.rb
@@ -3,81 +3,81 @@ require 'test/unit'
 
 class TestStatement < Test::Unit::TestCase
   def test_select
-    assert_sql 'SELECT 1', SQL::Statement::Select.new(SQL::Statement::Integer.new(1))
-    assert_sql 'SELECT * FROM users', SQL::Statement::Select.new(SQL::Statement::All.new, SQL::Statement::TableExpression.new(SQL::Statement::FromClause.new(SQL::Statement::Table.new('users'))))
+    assert_sql 'SELECT 1', select(int(1))
+    assert_sql 'SELECT * FROM users', select(all, tblx(from(tbl('users'))))
   end
 
   def test_select_list
-    assert_sql 'id', SQL::Statement::SelectList.new(SQL::Statement::Column.new('id'))
-    assert_sql 'id, name', SQL::Statement::SelectList.new([SQL::Statement::Column.new('id'), SQL::Statement::Column.new('name')])
+    assert_sql 'id', slist(col('id'))
+    assert_sql 'id, name', slist([col('id'), col('name')])
   end
 
   def test_distinct
-    assert_sql 'DISTINCT(username)', SQL::Statement::Distinct.new(SQL::Statement::Column.new('username'))
+    assert_sql 'DISTINCT(username)', distinct(col('username'))
   end
 
   def test_all
-    assert_sql '*', SQL::Statement::All.new
+    assert_sql '*', all
   end
 
   def test_table_expression
-    assert_sql 'FROM users WHERE id = 1 GROUP BY name', SQL::Statement::TableExpression.new(SQL::Statement::FromClause.new(SQL::Statement::Table.new('users')), SQL::Statement::WhereClause.new(SQL::Statement::Equals.new(SQL::Statement::Column.new('id'), SQL::Statement::Integer.new(1))), SQL::Statement::GroupByClause.new(SQL::Statement::Column.new('name')))
+    assert_sql 'FROM users WHERE id = 1 GROUP BY name', tblx(from(tbl('users')), where(equals(col('id'), int(1))), group_by(col('name')))
   end
 
   def test_from_clause
-    assert_sql 'FROM users', SQL::Statement::FromClause.new(SQL::Statement::Table.new('users'))
+    assert_sql 'FROM users', from(tbl('users'))
   end
 
   def test_full_outer_join
-    assert_sql 't1 FULL OUTER JOIN t2 ON t1.a = t2.a', SQL::Statement::FullOuterJoin.new(SQL::Statement::Table.new('t1'), SQL::Statement::Table.new('t2'), SQL::Statement::On.new(SQL::Statement::Equals.new(SQL::Statement::QualifiedColumn.new(SQL::Statement::Table.new('t1'), SQL::Statement::Column.new('a')), SQL::Statement::QualifiedColumn.new(SQL::Statement::Table.new('t2'), SQL::Statement::Column.new('a')))))
-    assert_sql 't1 FULL OUTER JOIN t2 USING (a)', SQL::Statement::FullOuterJoin.new(SQL::Statement::Table.new('t1'), SQL::Statement::Table.new('t2'), SQL::Statement::Using.new(SQL::Statement::Column.new('a')))
+    assert_sql 't1 FULL OUTER JOIN t2 ON t1.a = t2.a', SQL::Statement::FullOuterJoin.new(tbl('t1'), tbl('t2'), SQL::Statement::On.new(equals(qcol(tbl('t1'), col('a')), qcol(tbl('t2'), col('a')))))
+    assert_sql 't1 FULL OUTER JOIN t2 USING (a)', SQL::Statement::FullOuterJoin.new(tbl('t1'), tbl('t2'), SQL::Statement::Using.new(col('a')))
   end
 
   def test_full_join
-    assert_sql 't1 FULL JOIN t2 ON t1.a = t2.a', SQL::Statement::FullJoin.new(SQL::Statement::Table.new('t1'), SQL::Statement::Table.new('t2'), SQL::Statement::On.new(SQL::Statement::Equals.new(SQL::Statement::QualifiedColumn.new(SQL::Statement::Table.new('t1'), SQL::Statement::Column.new('a')), SQL::Statement::QualifiedColumn.new(SQL::Statement::Table.new('t2'), SQL::Statement::Column.new('a')))))
+    assert_sql 't1 FULL JOIN t2 ON t1.a = t2.a', SQL::Statement::FullJoin.new(tbl('t1'), tbl('t2'), SQL::Statement::On.new(equals(qcol(tbl('t1'), col('a')), qcol(tbl('t2'), col('a')))))
   end
 
   def test_right_outer_join
-    assert_sql 't1 RIGHT OUTER JOIN t2 ON t1.a = t2.a', SQL::Statement::RightOuterJoin.new(SQL::Statement::Table.new('t1'), SQL::Statement::Table.new('t2'), SQL::Statement::On.new(SQL::Statement::Equals.new(SQL::Statement::QualifiedColumn.new(SQL::Statement::Table.new('t1'), SQL::Statement::Column.new('a')), SQL::Statement::QualifiedColumn.new(SQL::Statement::Table.new('t2'), SQL::Statement::Column.new('a')))))
+    assert_sql 't1 RIGHT OUTER JOIN t2 ON t1.a = t2.a', SQL::Statement::RightOuterJoin.new(tbl('t1'), tbl('t2'), SQL::Statement::On.new(equals(qcol(tbl('t1'), col('a')), qcol(tbl('t2'), col('a')))))
   end
 
   def test_right_join
-    assert_sql 't1 RIGHT JOIN t2 ON t1.a = t2.a', SQL::Statement::RightJoin.new(SQL::Statement::Table.new('t1'), SQL::Statement::Table.new('t2'), SQL::Statement::On.new(SQL::Statement::Equals.new(SQL::Statement::QualifiedColumn.new(SQL::Statement::Table.new('t1'), SQL::Statement::Column.new('a')), SQL::Statement::QualifiedColumn.new(SQL::Statement::Table.new('t2'), SQL::Statement::Column.new('a')))))
+    assert_sql 't1 RIGHT JOIN t2 ON t1.a = t2.a', SQL::Statement::RightJoin.new(tbl('t1'), tbl('t2'), SQL::Statement::On.new(equals(qcol(tbl('t1'), col('a')), qcol(tbl('t2'), col('a')))))
   end
 
   def test_left_outer_join
-    assert_sql 't1 LEFT OUTER JOIN t2 ON t1.a = t2.a', SQL::Statement::LeftOuterJoin.new(SQL::Statement::Table.new('t1'), SQL::Statement::Table.new('t2'), SQL::Statement::On.new(SQL::Statement::Equals.new(SQL::Statement::QualifiedColumn.new(SQL::Statement::Table.new('t1'), SQL::Statement::Column.new('a')), SQL::Statement::QualifiedColumn.new(SQL::Statement::Table.new('t2'), SQL::Statement::Column.new('a')))))
+    assert_sql 't1 LEFT OUTER JOIN t2 ON t1.a = t2.a', SQL::Statement::LeftOuterJoin.new(tbl('t1'), tbl('t2'), SQL::Statement::On.new(equals(qcol(tbl('t1'), col('a')), qcol(tbl('t2'), col('a')))))
   end
 
   def test_left_join
-    assert_sql 't1 LEFT JOIN t2 ON t1.a = t2.a', SQL::Statement::LeftJoin.new(SQL::Statement::Table.new('t1'), SQL::Statement::Table.new('t2'), SQL::Statement::On.new(SQL::Statement::Equals.new(SQL::Statement::QualifiedColumn.new(SQL::Statement::Table.new('t1'), SQL::Statement::Column.new('a')), SQL::Statement::QualifiedColumn.new(SQL::Statement::Table.new('t2'), SQL::Statement::Column.new('a')))))
+    assert_sql 't1 LEFT JOIN t2 ON t1.a = t2.a', SQL::Statement::LeftJoin.new(tbl('t1'), tbl('t2'), SQL::Statement::On.new(equals(qcol(tbl('t1'), col('a')), qcol(tbl('t2'), col('a')))))
   end
 
   def test_inner_join
-    assert_sql 't1 INNER JOIN t2 ON t1.a = t2.a', SQL::Statement::InnerJoin.new(SQL::Statement::Table.new('t1'), SQL::Statement::Table.new('t2'), SQL::Statement::On.new(SQL::Statement::Equals.new(SQL::Statement::QualifiedColumn.new(SQL::Statement::Table.new('t1'), SQL::Statement::Column.new('a')), SQL::Statement::QualifiedColumn.new(SQL::Statement::Table.new('t2'), SQL::Statement::Column.new('a')))))
+    assert_sql 't1 INNER JOIN t2 ON t1.a = t2.a', SQL::Statement::InnerJoin.new(tbl('t1'), tbl('t2'), SQL::Statement::On.new(equals(qcol(tbl('t1'), col('a')), qcol(tbl('t2'), col('a')))))
   end
 
   def test_cross_join
-    assert_sql 't1 CROSS JOIN t2', SQL::Statement::CrossJoin.new(SQL::Statement::Table.new('t1'), SQL::Statement::Table.new('t2'))
-    assert_sql 't1 CROSS JOIN t2 CROSS JOIN t3', SQL::Statement::CrossJoin.new(SQL::Statement::CrossJoin.new(SQL::Statement::Table.new('t1'), SQL::Statement::Table.new('t2')), SQL::Statement::Table.new('t3'))
+    assert_sql 't1 CROSS JOIN t2', SQL::Statement::CrossJoin.new(tbl('t1'), tbl('t2'))
+    assert_sql 't1 CROSS JOIN t2 CROSS JOIN t3', SQL::Statement::CrossJoin.new(SQL::Statement::CrossJoin.new(tbl('t1'), tbl('t2')), tbl('t3'))
   end
 
   def test_order_clause
-    assert_sql 'ORDER BY name DESC', SQL::Statement::OrderClause.new(SQL::Statement::Descending.new(SQL::Statement::Column.new('name')))
-    assert_sql 'ORDER BY id ASC, name DESC', SQL::Statement::OrderClause.new([SQL::Statement::Ascending.new(SQL::Statement::Column.new('id')), SQL::Statement::Descending.new(SQL::Statement::Column.new('name'))])
+    assert_sql 'ORDER BY name DESC', SQL::Statement::OrderClause.new(SQL::Statement::Descending.new(col('name')))
+    assert_sql 'ORDER BY id ASC, name DESC', SQL::Statement::OrderClause.new([SQL::Statement::Ascending.new(col('id')), SQL::Statement::Descending.new(col('name'))])
   end
 
   def test_having_clause
-    assert_sql 'HAVING id = 1', SQL::Statement::HavingClause.new(SQL::Statement::Equals.new(SQL::Statement::Column.new('id'), SQL::Statement::Integer.new(1)))
+    assert_sql 'HAVING id = 1', SQL::Statement::HavingClause.new(equals(col('id'), int(1)))
   end
 
   def test_group_by_clause
-    assert_sql 'GROUP BY name', SQL::Statement::GroupByClause.new(SQL::Statement::Column.new('name'))
-    assert_sql 'GROUP BY name, status', SQL::Statement::GroupByClause.new([SQL::Statement::Column.new('name'), SQL::Statement::Column.new('status')])
+    assert_sql 'GROUP BY name', group_by(col('name'))
+    assert_sql 'GROUP BY name, status', group_by([col('name'), col('status')])
   end
 
   def test_where_clause
-    assert_sql 'WHERE 1 = 1', SQL::Statement::WhereClause.new(SQL::Statement::Equals.new(SQL::Statement::Integer.new(1), SQL::Statement::Integer.new(1)))
+    assert_sql 'WHERE 1 = 1', where(equals(int(1), int(1)))
   end
 
   def test_or
@@ -89,119 +89,119 @@ class TestStatement < Test::Unit::TestCase
   end
 
   def test_is_not_null
-    assert_sql '1 IS NOT NULL', SQL::Statement::Not.new(SQL::Statement::Is.new(SQL::Statement::Integer.new(1), SQL::Statement::Null.new))
+    assert_sql '1 IS NOT NULL', SQL::Statement::Not.new(SQL::Statement::Is.new(int(1), SQL::Statement::Null.new))
   end
 
   def test_is_null
-    assert_sql '1 IS NULL', SQL::Statement::Is.new(SQL::Statement::Integer.new(1), SQL::Statement::Null.new)
+    assert_sql '1 IS NULL', SQL::Statement::Is.new(int(1), SQL::Statement::Null.new)
   end
 
   def test_not_like
-    assert_sql "'hello' NOT LIKE 'h%'", SQL::Statement::Not.new(SQL::Statement::Like.new(SQL::Statement::String.new('hello'), SQL::Statement::String.new('h%')))
+    assert_sql "'hello' NOT LIKE 'h%'", SQL::Statement::Not.new(SQL::Statement::Like.new(str('hello'), str('h%')))
   end
 
   def test_like
-    assert_sql "'hello' LIKE 'h%'", SQL::Statement::Like.new(SQL::Statement::String.new('hello'), SQL::Statement::String.new('h%'))
+    assert_sql "'hello' LIKE 'h%'", SQL::Statement::Like.new(str('hello'), str('h%'))
   end
 
   def test_not_in
-    assert_sql '1 NOT IN (1, 2, 3)', SQL::Statement::Not.new(SQL::Statement::In.new(SQL::Statement::Integer.new(1), [SQL::Statement::Integer.new(1), SQL::Statement::Integer.new(2), SQL::Statement::Integer.new(3)]))
+    assert_sql '1 NOT IN (1, 2, 3)', SQL::Statement::Not.new(SQL::Statement::In.new(int(1), [int(1), int(2), int(3)]))
   end
 
   def test_in
-    assert_sql '1 IN (1, 2, 3)', SQL::Statement::In.new(SQL::Statement::Integer.new(1), [SQL::Statement::Integer.new(1), SQL::Statement::Integer.new(2), SQL::Statement::Integer.new(3)])
+    assert_sql '1 IN (1, 2, 3)', SQL::Statement::In.new(int(1), [int(1), int(2), int(3)])
   end
 
   def test_not_between
-    assert_sql '2 NOT BETWEEN 1 AND 3', SQL::Statement::Not.new(SQL::Statement::Between.new(SQL::Statement::Integer.new(2), SQL::Statement::Integer.new(1), SQL::Statement::Integer.new(3)))
+    assert_sql '2 NOT BETWEEN 1 AND 3', SQL::Statement::Not.new(SQL::Statement::Between.new(int(2), int(1), int(3)))
   end
 
   def test_between
-    assert_sql '2 BETWEEN 1 AND 3', SQL::Statement::Between.new(SQL::Statement::Integer.new(2), SQL::Statement::Integer.new(1), SQL::Statement::Integer.new(3))
+    assert_sql '2 BETWEEN 1 AND 3', SQL::Statement::Between.new(int(2), int(1), int(3))
   end
 
   def test_gte
-    assert_sql '1 >= 1', SQL::Statement::GreaterOrEquals.new(SQL::Statement::Integer.new(1), SQL::Statement::Integer.new(1))
+    assert_sql '1 >= 1', SQL::Statement::GreaterOrEquals.new(int(1), int(1))
   end
 
   def test_lte
-    assert_sql '1 <= 1', SQL::Statement::LessOrEquals.new(SQL::Statement::Integer.new(1), SQL::Statement::Integer.new(1))
+    assert_sql '1 <= 1', SQL::Statement::LessOrEquals.new(int(1), int(1))
   end
 
   def test_gt
-    assert_sql '1 > 1', SQL::Statement::Greater.new(SQL::Statement::Integer.new(1), SQL::Statement::Integer.new(1))
+    assert_sql '1 > 1', SQL::Statement::Greater.new(int(1), int(1))
   end
 
   def test_lt
-    assert_sql '1 < 1', SQL::Statement::Less.new(SQL::Statement::Integer.new(1), SQL::Statement::Integer.new(1))
+    assert_sql '1 < 1', SQL::Statement::Less.new(int(1), int(1))
   end
 
   def test_not_equals
-    assert_sql '1 <> 1', SQL::Statement::Not.new(SQL::Statement::Equals.new(SQL::Statement::Integer.new(1), SQL::Statement::Integer.new(1)))
+    assert_sql '1 <> 1', SQL::Statement::Not.new(equals(int(1), int(1)))
   end
 
   def test_equals
-    assert_sql '1 = 1', SQL::Statement::Equals.new(SQL::Statement::Integer.new(1), SQL::Statement::Integer.new(1))
+    assert_sql '1 = 1', equals(int(1), int(1))
   end
 
   def test_sum
-    assert_sql 'SUM(messages_count)', SQL::Statement::Sum.new(SQL::Statement::Column.new('messages_count'))
+    assert_sql 'SUM(messages_count)', SQL::Statement::Sum.new(col('messages_count'))
   end
 
   def test_minimum
-    assert_sql 'MIN(age)', SQL::Statement::Minimum.new(SQL::Statement::Column.new('age'))
+    assert_sql 'MIN(age)', SQL::Statement::Minimum.new(col('age'))
   end
 
   def test_maximum
-    assert_sql 'MAX(age)', SQL::Statement::Maximum.new(SQL::Statement::Column.new('age'))
+    assert_sql 'MAX(age)', SQL::Statement::Maximum.new(col('age'))
   end
 
   def test_average
-    assert_sql 'AVG(age)', SQL::Statement::Average.new(SQL::Statement::Column.new('age'))
+    assert_sql 'AVG(age)', SQL::Statement::Average.new(col('age'))
   end
 
   def test_count
-    assert_sql 'COUNT(*)', SQL::Statement::Count.new(SQL::Statement::All.new)
+    assert_sql 'COUNT(*)', SQL::Statement::Count.new(all)
   end
 
   def test_table
-    assert_sql 'users', SQL::Statement::Table.new('users')
+    assert_sql 'users', tbl('users')
   end
 
   def test_qualified_column
-    assert_sql 'users.id', SQL::Statement::QualifiedColumn.new(SQL::Statement::Table.new('users'), SQL::Statement::Column.new('id'))
+    assert_sql 'users.id', qcol(tbl('users'), col('id'))
   end
 
   def test_column
-    assert_sql 'id', SQL::Statement::Column.new('id')
+    assert_sql 'id', col('id')
   end
 
   def test_as
-    assert_sql '1 AS a', SQL::Statement::As.new(SQL::Statement::Integer.new(1), SQL::Statement::Column.new('a'))
+    assert_sql '1 AS a', SQL::Statement::As.new(int(1), col('a'))
   end
 
   def test_multiply
-    assert_sql '(2 * 2)', SQL::Statement::Multiply.new(SQL::Statement::Integer.new(2), SQL::Statement::Integer.new(2))
+    assert_sql '(2 * 2)', SQL::Statement::Multiply.new(int(2), int(2))
   end
 
   def test_divide
-    assert_sql '(2 / 2)', SQL::Statement::Divide.new(SQL::Statement::Integer.new(2), SQL::Statement::Integer.new(2))
+    assert_sql '(2 / 2)', SQL::Statement::Divide.new(int(2), int(2))
   end
 
   def test_add
-    assert_sql '(2 + 2)', SQL::Statement::Add.new(SQL::Statement::Integer.new(2), SQL::Statement::Integer.new(2))
+    assert_sql '(2 + 2)', SQL::Statement::Add.new(int(2), int(2))
   end
 
   def test_subtract
-    assert_sql '(2 - 2)', SQL::Statement::Subtract.new(SQL::Statement::Integer.new(2), SQL::Statement::Integer.new(2))
+    assert_sql '(2 - 2)', SQL::Statement::Subtract.new(int(2), int(2))
   end
 
   def test_unary_plus
-    assert_sql '+1', SQL::Statement::UnaryPlus.new(SQL::Statement::Integer.new(1))
+    assert_sql '+1', SQL::Statement::UnaryPlus.new(int(1))
   end
 
   def test_unary_minus
-    assert_sql '-1', SQL::Statement::UnaryMinus.new(SQL::Statement::Integer.new(1))
+    assert_sql '-1', SQL::Statement::UnaryMinus.new(int(1))
   end
 
   def test_true
@@ -225,10 +225,10 @@ class TestStatement < Test::Unit::TestCase
   end
 
   def test_string
-    assert_sql "'foo'", SQL::Statement::String.new('foo')
+    assert_sql "'foo'", str('foo')
 
     # # FIXME
-    # assert_sql "'O\\\'rly'", SQL::Statement::String.new("O'rly")
+    # assert_sql "'O\\\'rly'", str("O'rly")
   end
 
   def test_float
@@ -236,12 +236,68 @@ class TestStatement < Test::Unit::TestCase
   end
 
   def test_integer
-    assert_sql '1', SQL::Statement::Integer.new(1)
+    assert_sql '1', int(1)
   end
 
   private
 
   def assert_sql(expected, ast)
     assert_equal expected, ast.to_sql
+  end
+
+  def qcol(table, column)
+    SQL::Statement::QualifiedColumn.new(table, column)
+  end
+
+  def equals(left, right)
+    SQL::Statement::Equals.new(left, right)
+  end
+
+  def all
+    SQL::Statement::All.new
+  end
+
+  def str(value)
+    SQL::Statement::String.new(value)
+  end
+
+  def int(value)
+    SQL::Statement::Integer.new(value)
+  end
+
+  def col(name)
+    SQL::Statement::Column.new(name)
+  end
+
+  def tbl(name)
+    SQL::Statement::Table.new(name)
+  end
+
+  def distinct(col)
+    SQL::Statement::Distinct.new(col)
+  end
+
+  def slist(ary)
+    SQL::Statement::SelectList.new(ary)
+  end
+
+  def select(list, table_expression = nil)
+    SQL::Statement::Select.new(list, table_expression)
+  end
+
+  def tblx(from_clause, where_clause = nil, group_by_clause = nil, having_clause = nil)
+    SQL::Statement::TableExpression.new(from_clause, where_clause, group_by_clause, having_clause)
+  end
+
+  def from(tables)
+    SQL::Statement::FromClause.new(tables)
+  end
+
+  def where(search_condition)
+    SQL::Statement::WhereClause.new(search_condition)
+  end
+
+  def group_by(columns)
+    SQL::Statement::GroupByClause.new(columns)
   end
 end
